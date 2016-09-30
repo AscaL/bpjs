760cb2010ec96f12dcfd0c4c61ca09fdecc566cd
AscaL <emanueleorfanelli@gmail.com>
2016-09-30 13:04:43 +0200

    split index in two

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,4 @@
-import express from 'express';
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!!');
-});
-
-app.use((err, req, res) => {
-  console.error(err.stack);
-  res.status(500).send(err);
-});
+import app from './app'
 
 app.listen(8080, function() {
   const host = this.address().address;
