851a2eaac1786c93c166487fa98332412e38b44b
AscaL <emanueleorfanelli@gmail.com>
2016-11-22 13:20:11 +0200

    add db user and question

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,16 @@
 import app from './app';
 import { logger } from './util/logger';
+import {thinky} from './db';
 
-app.listen(8080, function () {
-  const host = this.address().address;
-  const port = this.address().port;
-  logger.info(`Experts server is listening at http://${host}:${port}`);
-});
+thinky.dbReady() .then(() => {
+  logger.info('Database ready, starting server...');
+  app.listen(8080, function () {
+    const host = this.address().address;
+    const port = this.address().port;
+    logger.info(`Experts server is listening at http://${host}:${port}`);
+  });
+})
 
-process.on('uncaughtExcepion', err => logger.error('err:', err));
-process.on('unhandledRejection', err => logger.error('err:', err));
+process.on('uncaughtExcepion', err => logger.error('uncaughtExcepion: ', err));
+process.on('unhandledRejection', err => logger.error('unhandledRejection: ', err));
 
